98bc535152b8342fb7900e6082ceb3bdd005c17d
pgerspach <pgerspac@alumni.nd.edu>
2019-03-02 08:04:49 -0500

    Updates

---
+++ precache-manifest.3d4930ed9600eae68f607c39e153ccc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "065ca38133e73aaf4baa",
+    "url": "/clicky-game/static/js/main.065ca381.chunk.js"
+  },
+  {
+    "revision": "713fac1b7cf6a175c225",
+    "url": "/clicky-game/static/js/2.713fac1b.chunk.js"
+  },
+  {
+    "revision": "065ca38133e73aaf4baa",
+    "url": "/clicky-game/static/css/main.0f3acb3e.chunk.css"
+  },
+  {
+    "revision": "b824411076a1fc6e253bf73e845952a5",
+    "url": "/clicky-game/index.html"
+  }
+];
